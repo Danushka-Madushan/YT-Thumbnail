45d8d2e0e46e64a5f7c951fbab91949fe1af74c0
Danushka-Madushan <69332746+Danushka-Madushan@users.noreply.github.com>
2022-01-12 00:35:50 +0530

    Update Main Document
    Download Engine is now updated and have fast error avoiding system. Fixed HD Resolution not support error!

--- Main.py
+++ Main.py
@@ -4,65 +4,59 @@ import sys
 import re 
 
 def Main(Quality):
-	for a in YTstrams:
+	for a in list(set(YTstrams)):
 		try:
 			response = requests.get(f"https://www.youtube.com/oembed?url={a}").json()
-			print(f"\n Title   : {response['title']}")
-			print(f" Channel : {response['author_name']}")
-
-			vid = re.search(r'https?://www.youtube.com/watch\?v=([A-z0-9-_]*)', a).group(1)
+			print("\n$ Title   : %s" % response['title'])
+			print("$ Channel : %s" % response['author_name'])
+			vid = re.search(r'v=(.+)', a).group(1)
 			links = {}
-			links["HD"] = (f"https://img.youtube.com/vi/{vid}/maxresdefault.jpg")
-			links["SD"] = (f"https://img.youtube.com/vi/{vid}/sddefault.jpg")
-			links["HQ"] = (f"https://img.youtube.com/vi/{vid}/hqdefault.jpg")
-			links["MQ"] = (f"https://img.youtube.com/vi/{vid}/mqdefault.jpg")
-
+			links["HD"] = ("https://img.youtube.com/vi/%s/maxresdefault.jpg" % vid)
+			links["SD"] = ("https://img.youtube.com/vi/%s/sddefault.jpg" % vid)
+			links["HQ"] = ("https://img.youtube.com/vi/%s/hqdefault.jpg" % vid)
+			links["MQ"] = ("https://img.youtube.com/vi/%s/mqdefault.jpg" % vid)
 			try:
-				image_url = links[f'{Quality}']
-				if re.finditer(r'[/\\|":*?]', response['title']):
-					title = vid
-				else:
-					title = response['title']
-				filename = (f"{title} ({Quality}).jpg")
-				print(" Downloading...")
+				image_url = links[Quality]
+				strlst = r'/\:*?"<>|'
+				for item in strlst:
+					response['title'] = response['title'].replace(item, '')
+				filename = ("%s (%s).jpg" % (response['title'], Quality))
+				print("$ Downloading... (%s)" % Quality)
 				resx = int(requests.get(image_url, stream=True).headers['Content-length'])
 				urllib.request.urlretrieve(image_url, filename)
 				size = resx/1024
-				print(f' Successfully Dowloaded ({round(size, 2)} KB) ')
+				print("$ Successfully Dowloaded (%i KB)" % round(size, 2))
 			except Exception as Download_Error:
 			    if str(Download_Error) == "HTTP Error 404: Not Found":
-			    	print(f" {Quality} Source not Available! Try Using Different Quality...")
-			    	print(f" Link : {a}")
-
-			    else:
-			    	print(" Error With Video Name! Retrying Using Video ID...")
-			    	urllib.request.urlretrieve(image_url, f"{vid}.jpg")
+			    	print("$ %s Source not available Downloading in HQ..." % Quality)
+			    	filename = ("%s (%s).jpg" % (response['title'], 'HQ'))
+			    	resx = int(requests.get(links['HQ'], stream=True).headers['Content-length'])
+			    	print("$ Downloading... (%s)" % 'HQ')
+			    	urllib.request.urlretrieve(links['HQ'], filename)
 			    	size = resx/1024
-			    	print(f' Successfully Dowloaded ({round(size, 2)} KB) ')
-
+			    	print("$ Successfully Dowloaded (%i KB)" % round(size, 2))
+			    	pass
 		except Exception:
-			return (f"\n Video Link Does not Exist : {a} \n")
+			print("\n$ Video Link Does not Exist : %s" % a)
+			pass
 
 if __name__ == "__main__":
 	YTstrams = []
 	while True:
-		_link = input("\n YouTube Link : ")
+		_link = input("\n$ YouTube Link : ")
 		if len(_link) == 0:
 			break 
 		else:
-			if re.match(r'https://www.youtube.com/watch\?v=[A-z0-9-_]*', _link):
+			if re.match(r'^https?://w{3}\.youtube\.com/watch\?v=(.+)$', _link):
 				YTstrams.append(_link)
 				continue
 			else:
-				print(f" Invalid YouTube Link : {_link}")
 				continue
 	QY = input("\n Quality ( HD SD HQ MQ ) : ").upper()
 	if len(QY) == 2:
 		if QY == str("HD") or str("SD") or str("HQ") or str("MQ"):
 			Main(QY)
 		else:
-			print(" Invalid Quality :/")
 			sys.exit()
 	else:
-		print(" Invalid Quality :/")
 		sys.exit()
